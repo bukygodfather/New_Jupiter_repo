6384eb1d1edca67d877ec1e070d66ebe1d92ed46
Ebuka Frank <ebukafrank@gmail.com>
2025-01-02 08:45:40 +0000

    added new files

--- mars1.0/cypress/e2e/CENIDEA/navBar.cy.ts
+++ mars1.0/cypress/e2e/CENIDEA/navBar.cy.ts
@@ -3,7 +3,7 @@ describe("Navigation:coaching", () => {
 
   
     it("Click dropdown menu", () => {
-      cy.visit("https://cenidea-git-develop-cenidea.vercel.app/");
+      cy.visit("/");
       cy.get('button[id="rcc-confirm-button"]').should('exist').click({force:true});
       cy.get('a[id="navbarDropdownMenuLink"]').click({force:true, multiple:true});
       cy.get('a[href*="/coaching/executive-coaching"]').click({multiple:true});
